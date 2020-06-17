4844c09a946efe2c87f646df9c980b16fb838bbc
Khanh <khanh@vainu.io>
2020-06-17 22:13:43 +0300

    add settings module

---
+++ settings.py
@@ -0,0 +1,10 @@
+"""Module for project hyper parameters"""
+
+ENV_PATH = '/data/Reacher_One_Linux_NoVis/Reacher_One_Linux_NoVis.x86_64'
+ACTOR_NETWORK_LINEAR_SIZES = "1024,512,256"
+CRITIC_NETWORK_LINEAR_SIZES = "1024,200"
+LEARNING_RATE = 1e-3
+BUFFER_SIZE = 1e5
+BATCH_SIZE = 256
+UPDATE_EVERY = 10
+GAMMA = 0.99            
