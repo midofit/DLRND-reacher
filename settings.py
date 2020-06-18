b41e55bd8710b9e5098dfcf2794808cb0e163fea
Khanh <khanh@vainu.io>
2020-06-18 19:09:57 +0300

    update BUFFER_SIZE

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 """Module for project hyper parameters"""
 
-ENV_PATH = './data/Reacher_One_Linux_NoVis.x86_64'
+ENV_PATH = './data/Reacher.app'
 ACTOR_NETWORK_LINEAR_SIZES = "1024,512,256"
 CRITIC_NETWORK_LINEAR_SIZES = "1024,200"
 LEARNING_RATE = 1e-3
-BUFFER_SIZE = 1e5       # replay buffer size
+BUFFER_SIZE = int(1e5)       # replay buffer size
 BATCH_SIZE = 256        # minibatch size
 UPDATE_EVERY = 10       # how often to update the network
 GAMMA = 0.99            # discount factor
