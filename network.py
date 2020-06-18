9f33e1bdc043cdce68e4f05e0fe7ae0b5f2da2c9
Khanh <khanh@vainu.io>
2020-06-18 11:32:24 +0300

    update Critic network, add action to forward pass and change output size to match action size

--- network.py
+++ network.py
@@ -37,7 +37,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size: int, seed:float, linear_sizes:str, dropout:float=0.1):
+    def __init__(self, state_size: int, action_size:int, seed:float, linear_sizes:str, dropout:float=0.1):
         """Initialize parameters and build model.
         Params
         ======
@@ -50,16 +50,17 @@ class Critic(nn.Module):
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=dropout)
         linear_layer_sizes = linear_sizes.split(",")
-        linear_layers = [nn.Linear(state_size, int(linear_layer_sizes[0])), self.relu, self.dropout]
+        linear_layers = [nn.Linear(state_size + action_size, int(linear_layer_sizes[0])), self.relu, self.dropout]
         for i in range(1, len(linear_layer_sizes)):
             linear_layer_size = linear_layer_sizes[i]
             linear_layer = nn.Linear(int(linear_layer_sizes[i-1]), int(linear_layer_size))
             linear_layers.extend([linear_layer, self.relu, self.dropout])
         self.linear = nn.Sequential(*linear_layers)
-        self.output = nn.Linear(int(linear_layer_sizes[-1]), 1)
+        self.output = nn.Linear(int(linear_layer_sizes[-1]), action_size)
 
-    def forward(self, state):
+    def forward(self, state, action):
         """Build a network that maps state -> action values."""
-        output = self.linear(state)
+        output = torch.cat([state, action], dim=1)
+        output = self.linear(output)
         output = self.output(output)
         return output
