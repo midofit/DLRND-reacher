d9a869bb19d437a21b63929607b397ea082976c8
Khanh <khanh@vainu.io>
2020-06-18 15:22:23 +0300

    add new parameter TAU for controlling soft update process

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 """Module for project hyper parameters"""
 
-ENV_PATH = '/data/Reacher_One_Linux_NoVis/Reacher_One_Linux_NoVis.x86_64'
+ENV_PATH = './data/Reacher_One_Linux_NoVis.x86_64'
 ACTOR_NETWORK_LINEAR_SIZES = "1024,512,256"
 CRITIC_NETWORK_LINEAR_SIZES = "1024,200"
 LEARNING_RATE = 1e-3
-BUFFER_SIZE = 1e5
-BATCH_SIZE = 256
-UPDATE_EVERY = 10
-GAMMA = 0.99            
+BUFFER_SIZE = 1e5       # replay buffer size
+BATCH_SIZE = 256        # minibatch size
+UPDATE_EVERY = 10       # how often to update the network
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
