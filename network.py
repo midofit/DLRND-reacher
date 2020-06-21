1dcb9e693f252ba89b1297ee0683692c12787023
Khanh <khanh@vainu.io>
2020-06-21 10:53:56 +0300

    apply batch normalization to every layer in critic and actor networks

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size: int, action_size:int, seed:float, linear_sizes:str, dropout:float=0.1):
+    def __init__(self, state_size: int, action_size:int, seed:float, linear_sizes:str, dropout:float=0.32):
         """Initialize parameters and build model.
         Params
         ======
@@ -18,11 +18,12 @@ class Actor(nn.Module):
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=dropout)
         linear_layer_sizes = linear_sizes.split(",")
-        linear_layers = [nn.Linear(state_size, int(linear_layer_sizes[0])), self.relu, self.dropout]
+        linear_layers = [nn.BatchNorm1d(state_size), nn.Linear(state_size, int(linear_layer_sizes[0])), self.relu, self.dropout]
         for i in range(1, len(linear_layer_sizes)):
+            batch_norm = nn.BatchNorm1d(int(linear_layer_sizes[i-1]))
             linear_layer_size = linear_layer_sizes[i]
             linear_layer = nn.Linear(int(linear_layer_sizes[i-1]), int(linear_layer_size))
-            linear_layers.extend([linear_layer, self.relu, self.dropout])
+            linear_layers.extend([batch_norm, linear_layer, self.relu, self.dropout])
         self.linear = nn.Sequential(*linear_layers)
         self.output = nn.Linear(int(linear_layer_sizes[-1]), action_size)
 
@@ -38,7 +39,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size: int, action_size:int, seed:float, linear_sizes:str, dropout:float=0.1):
+    def __init__(self, state_size: int, action_size:int, seed:float, linear_sizes:str, dropout:float=0.32):
         """Initialize parameters and build model.
         Params
         ======
@@ -51,11 +52,12 @@ class Critic(nn.Module):
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=dropout)
         linear_layer_sizes = linear_sizes.split(",")
-        linear_layers = [nn.Linear(state_size + action_size, int(linear_layer_sizes[0])), self.relu, self.dropout]
+        linear_layers = [nn.BatchNorm1d(state_size + action_size), nn.Linear(state_size + action_size, int(linear_layer_sizes[0])), self.relu, self.dropout]
         for i in range(1, len(linear_layer_sizes)):
+            batch_norm = nn.BatchNorm1d(int(linear_layer_sizes[i-1]))
             linear_layer_size = linear_layer_sizes[i]
             linear_layer = nn.Linear(int(linear_layer_sizes[i-1]), int(linear_layer_size))
-            linear_layers.extend([linear_layer, self.relu, self.dropout])
+            linear_layers.extend([batch_norm, linear_layer, self.relu, self.dropout])
         self.linear = nn.Sequential(*linear_layers)
         self.output = nn.Linear(int(linear_layer_sizes[-1]), action_size)
 
