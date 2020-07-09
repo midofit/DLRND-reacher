c41ccb482f2b02fb4a92ec55fa8dad3ba1065b09
Khanh <khanh@vainu.io>
2020-07-09 13:51:05 +0300

    add new parameter LEARN_TIMES to control number of times the agent learn for each learning session

--- settings.py
+++ settings.py
@@ -1,18 +1,18 @@
 """Module for project hyper parameters"""
 
-ENV_PATH = './data/Reacher.app'
-ACTOR_NETWORK_LINEAR_SIZES = "300,200"
-CRITIC_NETWORK_LINEAR_SIZES = "200,100"
-ACTOR_LEARNING_RATE = 1e-3
-CRITIC_LEARNING_RATE = 1e-3
-BUFFER_SIZE = int(1e5)          # replay buffer size
-BATCH_SIZE = 128                 # minibatch size
-UPDATE_EVERY = 1                # how often to update the network
+ENV_PATH = './data/Reacher2.app'
+ACTOR_NETWORK_LINEAR_SIZES = "512,384"
+CRITIC_NETWORK_LINEAR_SIZES = "512,384"
+ACTOR_LEARNING_RATE = 5e-4
+CRITIC_LEARNING_RATE = 5e-4
+BUFFER_SIZE = int(2e6)          # replay buffer size
+BATCH_SIZE = 512                # minibatch size
+UPDATE_EVERY = 20               # how often to update the network
 GAMMA = 0.99                    # discount factor
 TAU = 1e-3                      # for soft update of target parameters
 CRITIC_BATCH_NORM = True        # apply batch norm for critic network
 ACTOR_BATCH_NORM = True         # apply batch norm for actor network
-
+LEARN_TIMES = 10
 
 def printvars():
    tmp = globals().copy()
