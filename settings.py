7b19f700421a284cfced3b78ce13a5ceaf66fe03
Khanh Nguyen <khanh1417@gmail.com>
2020-07-28 17:29:10 +0300

    update latest settings

--- settings.py
+++ settings.py
@@ -1,21 +1,18 @@
 """Module for project hyper parameters"""
 
-ENV_PATH = './data/Reacher2.app'
-ACTOR_NETWORK_LINEAR_SIZES = "512,384"
-CRITIC_NETWORK_LINEAR_SIZES = "512,384"
-ACTOR_LEARNING_RATE = 5e-4
+ENV_PATH = './data/Reacher_2/Reacher.exe'
+ACTOR_LEARNING_RATE = 1e-3
 CRITIC_LEARNING_RATE = 1e-3
-BUFFER_SIZE = int(2e6)          # replay buffer size
-BATCH_SIZE = 512                # minibatch size
-UPDATE_EVERY = 20               # how often to update the network
+BUFFER_SIZE = int(1e6)          # replay buffer size
+BATCH_SIZE = 256               # minibatch size
+UPDATE_EVERY = 2               # how often to update the network
 GAMMA = 0.99                    # discount factor
 TAU = 1e-3                      # for soft update of target parameters
 CRITIC_BATCH_NORM = True        # apply batch norm for critic network
 ACTOR_BATCH_NORM = True         # apply batch norm for actor network
-LEARN_TIMES = 10
+LEARN_TIMES = 1
 CRITIC_GRADIENT_CLIPPING_VALUE = 1
-ACTOR_GRADIENT_CLIPPING_VALUE = 1
-
+ACTOR_GRADIENT_CLIPPING_VALUE = 0
 
 def printvars():
    tmp = globals().copy()
